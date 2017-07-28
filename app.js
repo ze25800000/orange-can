17452f28a59b8a87f619a40b8e2e85c3b94b0eb5
ze25800000 <ze258100000@sina.com>
2017-07-28 10:17:40 +0800

    11.3-page275：数据缓存的异步操作

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 App({
-    // onLaunch() {
-    //     var storageData = wx.getStorageSync('postList');
-    //     if (!storageData) {
-    //         var dataObj = require('data/data');
-    //         wx.clearStorageSync();
-    //     }
-    //     wx.setStorageSync('postList', dataObj.postList);
-    // },
+    onLaunch() {
+        // var storageData = wx.getStorageSync('postList');
+        // if (!storageData) {
+        //     var dataObj = require('data/data');
+        //     wx.clearStorageSync();
+        // }
+        // wx.setStorageSync('postList', dataObj.postList);
+        this._getUserInfo();
+    },
     globalData: {
         g_isPlayingMusic: false,
         g_currentMusicPostId: null,
-        doubanBase:"https://api.douban.com"
+        doubanBase: "https://api.douban.com",
+        g_userInfo: null
+    },
+    _getUserInfo() {
+        let userInfoStorage = wx.getStorageSync('user');
+        if (!userInfoStorage) {
+            //如果缓存中没有用户信息，那么获取用户信息
+            let that = this;
+            wx.login({
+                success() {
+                    wx.getUserInfo({
+                        success(res) {
+                            that.globalData.g_userInfo = res.userInfo;
+                            //将用户的基本信息保存在缓存中
+                            wx.setStorageSync('user', res.userInfo);
+                        },
+                        fail(res) {
+                            console.log(res);
+                        }
+                    });
+                }
+            })
+        } else {
+            this.globalData.g_userInfo = userInfoStorage;
+        }
     }
 });
--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -1,29 +1,29 @@
-// setting.js
+let app = getApp();
 Page({
     data: {
         cache: [
-            { iconurl: '/images/icon/wx_app_clear.png', title: '缓存清理', tap: 'clearCache' }
+            {iconurl: '/images/icon/wx_app_clear.png', title: '缓存清理', tap: 'clearCache'}
         ],
         device: [
-            { iconurl: '/images/icon/wx_app_cellphone.png', title: '系统信息', tap: 'showSystemInfo' },
-            { iconurl: '/images/icon/wx_app_network.png', title: '网络状态', tap: 'showNetWork' },
-            { iconurl: '/images/icon/wx_app_location.png', title: '地图显示', tap: 'showMap' },
-            { iconurl: '/images/icon/wx_app_compass.png', title: '指南针', tap: 'showCompass' },
-            { iconurl: '/images/icon/wx_app_lonlat.png', title: '当前位置、速度', tap: 'showLonLat' },
-            { iconurl: '/images/icon/wx_app_shake.png', title: '摇一摇', tap: 'shake' },
-            { iconurl: '/images/icon/wx_app_scan_code.png', title: '二维码', tap: 'scanQRCode' }
+            {iconurl: '/images/icon/wx_app_cellphone.png', title: '系统信息', tap: 'showSystemInfo'},
+            {iconurl: '/images/icon/wx_app_network.png', title: '网络状态', tap: 'showNetWork'},
+            {iconurl: '/images/icon/wx_app_location.png', title: '地图显示', tap: 'showMap'},
+            {iconurl: '/images/icon/wx_app_compass.png', title: '指南针', tap: 'showCompass'},
+            {iconurl: '/images/icon/wx_app_lonlat.png', title: '当前位置、速度', tap: 'showLonLat'},
+            {iconurl: '/images/icon/wx_app_shake.png', title: '摇一摇', tap: 'shake'},
+            {iconurl: '/images/icon/wx_app_scan_code.png', title: '二维码', tap: 'scanQRCode'}
         ],
         api: [
-            { iconurl: '/images/icon/wx_app_list.png', title: '下载pdf、word文档', tap: 'downloadDocumentList' },
-            { iconurl: '', title: '用户登陆', tap: 'login' },
-            { iconurl: '', title: '校验用户信息', tap: 'check' },
-            { iconurl: '', title: '获取用户加密信息', tap: 'decrypted' },
-            { iconurl: '', title: '模板消息', tap: 'tplMessage' },
-            { iconurl: '', title: '微信支付', tap: 'wxPay' }
+            {iconurl: '/images/icon/wx_app_list.png', title: '下载pdf、word文档', tap: 'downloadDocumentList'},
+            {iconurl: '', title: '用户登陆', tap: 'login'},
+            {iconurl: '', title: '校验用户信息', tap: 'check'},
+            {iconurl: '', title: '获取用户加密信息', tap: 'decrypted'},
+            {iconurl: '', title: '模板消息', tap: 'tplMessage'},
+            {iconurl: '', title: '微信支付', tap: 'wxPay'}
         ],
         others: [
-            { iconurl: '', title: 'wx:key示例', tap: 'showWxKeyDemo' },
-            { iconurl: '', title: 'scroll-view高级用法演示', tap: 'showScrollViewDemo' }
+            {iconurl: '', title: 'wx:key示例', tap: 'showWxKeyDemo'},
+            {iconurl: '', title: 'scroll-view高级用法演示', tap: 'showScrollViewDemo'}
         ],
         compassVal: 0,
         compassHidden: true,
@@ -39,6 +39,39 @@ Page({
         }
     },
     onLoad: function (options) {
-
+        this.setData({
+            userInfo: app.globalData.g_userInfo
+        });
     },
+    showModal(title, content, callback) {
+        wx.showModal({
+            title: title,
+            content: content,
+            confirmColor: '#1f4ba5',
+            cancelColor: '#7f8389',
+            success(res) {
+                if (res.confirm) {
+                    callback && callback();
+                }
+            }
+        })
+    },
+    //缓存清理
+    clearCache() {
+        this.showModal('缓存清楚', '确定要清楚本地缓存么？', function () {
+            wx.clearStorage({
+                success(msg) {
+                    wx.showToast({
+                        title: '缓存清楚成功',
+                        duration: 1000,
+                        mask: true,
+                        icon: "success"
+                    });
+                },
+                fail(e) {
+                    console.log(e);
+                }
+            });
+        })
+    }
 });
